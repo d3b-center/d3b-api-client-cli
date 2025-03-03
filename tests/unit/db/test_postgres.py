9f7c32717e18056eb4128eff26dc22ca48cd90cf
Natasha Singh <dukedesi22@gmail.com>
2025-03-03 12:41:02 -0500

    ♻️ Check db conn params for blank str (#13)
    * :recycle: Check for empty str and None in db conn params
    
    * :white_check_mark: Unit test db conn params
    
    * :rotating_light: Fix black formatter errors

---
+++ tests/unit/db/test_postgres.py
@@ -0,0 +1,52 @@
+"""
+Unit tests for interacting with postgres
+"""
+
+import pytest
+
+from d3b_api_client_cli.db.postgres import DBConnectionParam
+
+
+def test_db_connection_params():
+    """
+    Test DBConnectionParam
+    """
+    username = "foo"
+    password = "bar"
+    hostname = "postgres.com"
+    port = "5432"
+    db_name = "postgres"
+
+    params = DBConnectionParam(
+        username=username,
+        password=password,
+        hostname=hostname,
+        port=port,
+        db_name=db_name,
+    )
+
+    assert params.username == username
+    assert params.password == password
+    assert params.hostname == hostname
+    assert params.port == port
+    assert params.db_name == db_name
+
+
+def test_db_connection_params_error():
+    """
+    Test DBConnectionParam errors
+    """
+    username = ""
+    password = ""
+    hostname = None
+    port = None
+    db_name = "postgres"
+
+    with pytest.raises(ValueError):
+        DBConnectionParam(
+            username=username,
+            password=password,
+            hostname=hostname,
+            port=port,
+            db_name=db_name,
+        )
